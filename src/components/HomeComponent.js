287ad4d4ef0175db55be09d28a0f550880968362
virat02 <virat.goradia96@gmail.com>
2019-06-19 13:13:56 -0400

    Rectified css per movie card

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,13 +9,48 @@ export default class HomeComponent extends Component {
 
     render() {
         return(
-            <div className="container overlay">
-                <img
-                    src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg="
-                    alt=""/>
-                <p className="title">card title</p>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="movie-card">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg="
+                                        alt=""/>
+                                    <p className="title">card title</p>
+                                    <div className="button"><a href="#"> BUTTON </a></div>
+                            </div>
+                        </div>
 
-                <div className="button"><a href="#"> BUTTON </a></div>
-            </div>
+                        <div className="col-md-4">
+                            <div className="movie-card">
+                                <img
+                                    src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg="
+                                    alt=""/>
+                                <p className="title">card title</p>
+                                <div className="button"><a href="#"> BUTTON </a></div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="movie-card">
+                                <img
+                                    src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg="
+                                    alt=""/>
+                                <p className="title">card title</p>
+                                <div className="button"><a href="#"> BUTTON </a></div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="movie-card">
+                                <img
+                                    src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg="
+                                    alt=""/>
+                                <p className="title">card title</p>
+                                <div className="button"><a href="#"> BUTTON </a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
     )}
 }
